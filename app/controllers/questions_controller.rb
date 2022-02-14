4ab07a94e1eac78af6e4f77bd0c3c12387e12cd4
florian bel <florian-bel@hotmail.fr>
2022-02-14 14:13:59 +0100

    Exo 1 rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+    # render views/questions/ask.html.erb
+  end
+
+  def answer
+    @question = params[:question]
+    if params[:question] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:question].include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    # render views/questions/answer.html.erb
+  end
+end
